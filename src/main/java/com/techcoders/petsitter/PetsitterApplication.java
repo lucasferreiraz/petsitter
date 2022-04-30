dbd9480e03b04e38f997d684a90d6b6a130c7ac3
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-04-30 19:00:57 -0300

    Initial commit

---
+++ src/main/java/com/techcoders/petsitter/PetsitterApplication.java
@@ -0,0 +1,13 @@
+package com.techcoders.petsitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcoders/petsitter/PetsitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcoders.petsitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
