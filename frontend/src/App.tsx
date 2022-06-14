9c62c8c2313bedd7cef8b3e86d916cbb8d741170
kaikySantos <info.kaiky@gmail.com>
2022-06-14 20:04:33 -0300

    feat: estrutura básica

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">
+        Olá
+      </div>
+    </>
+  );
+}
